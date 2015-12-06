52ffa60bd3537508d727ba12e2cfc37d3edab320
tiffanyposs <tiffanyposs@gmail.com>
2015-12-06 01:08:30 -0500

    Desktop version and express server

---
+++ app.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var ejs = require('ejs');
+
+var app = express();
+
+app.set('views', './views');
+app.set('view engine', 'ejs')
+
+;
+app.use(express.static('public'));
+
+	app.get('/', function (req, res) {
+	  	res.render('index.ejs')
+	});
+
+	app.get('/:id', function (req, res) {
+		var fileName = req.params.id;
+	  	res.render(fileName)
+	})
+
+
+	app.use(function(req, res, next) {
+	    var err = new Error('Not Found');
+	    err.status = 404;
+	    next(err);
+	});
+
+
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+
+
+
+	// // development error handler
+	// // will print stacktrace
+	// if (app.get('env') === 'development') {
+	//     app.use(function(err, req, res, next) {
+	//         res.status(err.status || 500);
+	//         res.render('error', {
+	//             message: err.message,
+	//             error: err
+	//         });
+	//     });
+	// }
+
+	// // production error handler
+	// // no stacktraces leaked to user
+	// app.use(function(err, req, res, next) {
+	//     res.status(err.status || 500);
+	//     res.render('error', {
+	//         message: err.message,
+	//         error: {}
+	//     });
+	// });
+
+
+
+app.listen(8081)
+
+console.log("I love motorcycles on port 8081")
+
+// exports = module.exports = app;
---
+++ public/javascripts/javascript.js
@@ -0,0 +1,87 @@
+$(document).ready(function(){
+
+
+  var makeHeight = function(){
+	  var imageHeight = $('.image img').height()
+	  $('.approach-images').css('height', imageHeight)
+  }
+
+  $( window ).resize(function() {
+    makeHeight()
+  });
+
+  makeHeight()
+
+
+//nav bar animation
+var animatedScroll = false;
+
+$(window).scroll(function(){
+    var position = $(window).scrollTop()
+    if (!animatedScroll || position < 100) {
+        animatedScroll = true;
+        if  (position < 100){
+
+          $('header nav').removeClass('shownav');
+    			$( "header nav img" ).stop().animate({
+    			   height: '150px',
+    			}, 500, "linear", function(){animatedScroll = false; });
+
+        } else {
+
+        $('header nav').addClass('shownav')
+	  		$( "header nav img" ).stop().animate({
+		   	 	height: '100px',
+		  	}, 500, "linear", function(){animatedScroll = false; })
+        }
+    }
+});
+
+
+
+//controls the hover
+$('.approach-images .image img').mouseover(function(){
+  var height = $(this).height();
+  var $cover = $(this).parent().children('div')[0];
+
+  $($cover).animate({
+    height: height
+  }, 500, "linear")
+
+})
+
+$('.approach-images .image > div:nth-child(2)').mouseleave(function(){
+  $(this).animate({
+    height: "0px"
+  }, 500, "linear")
+})
+
+
+// $('.approach-images .image img').on("mouseenter", function(event) {
+//   var pin = $(event.target);
+//   pin.addClass("hovered");
+
+//   var height = $(this).height();
+//   var $cover = $(this).parent().children('div')[0];
+
+//   $($cover).animate({
+//     height: height
+//   }, 500, "linear")
+
+//   $(window).bind("mousemove", function(event) {
+//     var target = $(event.target);
+//     if (!target.hasClass("hovered")) {
+
+//       $($cover).animate({
+//         height: "0px"
+//       }, 500, "linear")
+
+//       $(".hovered").removeClass("hovered");
+//       $(window).unbind("mousemove");
+//     }
+//   })
+// })
+
+
+
+})
