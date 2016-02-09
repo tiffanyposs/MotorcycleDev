b33094f1f247b5d0966a97062a5a6d491279004f
tiffanyposs <tiffanyposs@gmail.com>
2016-02-08 21:46:52 -0500

    revising partials and file structure

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -13,74 +13,49 @@ $(document).ready(function(){
   makeHeight()
 
 
-//nav bar animation
-var animatedScroll = false;
+  //nav bar animation
+  var animatedScroll = false;
 
-$(window).scroll(function(){
-    var position = $(window).scrollTop()
-    if (!animatedScroll || position < 100) {
-        animatedScroll = true;
-        if  (position < 100){
+  $(window).scroll(function(){
+      var position = $(window).scrollTop()
+      if (!animatedScroll || position < 100) {
+          animatedScroll = true;
+          if  (position < 100){
 
-          $('header nav').removeClass('shownav');
-    			$( "header nav img" ).stop().animate({
-    			   height: '150px',
-    			}, 500, "linear", function(){animatedScroll = false; });
+            $('header nav').removeClass('shownav');
+      			$( "header nav img" ).stop().animate({
+      			   height: '150px',
+      			}, 500, "linear", function(){animatedScroll = false; });
 
-        } else {
+          } else {
 
-        $('header nav').addClass('shownav')
-	  		$( "header nav img" ).stop().animate({
-		   	 	height: '100px',
-		  	}, 500, "linear", function(){animatedScroll = false; })
-        }
-    }
-});
-
-
-
-//controls the hover
-$('.approach-images .image img').mouseover(function(){
-  var height = $(this).height();
-  var $cover = $(this).parent().children('div')[0];
-
-  $($cover).animate({
-    height: height
-  }, 500, "linear")
-
-})
-
-$('.approach-images .image > div:nth-child(2)').mouseleave(function(){
-  $(this).animate({
-    height: "0px"
-  }, 500, "linear")
-})
+          $('header nav').addClass('shownav')
+  	  		$( "header nav img" ).stop().animate({
+  		   	 	height: '100px',
+  		  	}, 500, "linear", function(){animatedScroll = false; })
+          }
+      }
+  });
 
 
-// $('.approach-images .image img').on("mouseenter", function(event) {
-//   var pin = $(event.target);
-//   pin.addClass("hovered");
 
-//   var height = $(this).height();
-//   var $cover = $(this).parent().children('div')[0];
+  //controls the hover
+  $('.approach-images .image img').mouseover(function(){
+    var height = $(this).height();
+    var $cover = $(this).parent().children('div')[0];
 
-//   $($cover).animate({
-//     height: height
-//   }, 500, "linear")
+    $($cover).animate({
+      height: height
+    }, 500, "linear")
 
-//   $(window).bind("mousemove", function(event) {
-//     var target = $(event.target);
-//     if (!target.hasClass("hovered")) {
+  })
 
-//       $($cover).animate({
-//         height: "0px"
-//       }, 500, "linear")
+  $('.approach-images .image > div:nth-child(2)').mouseleave(function(){
+    $(this).animate({
+      height: "0px"
+    }, 500, "linear")
+  })
 
-//       $(".hovered").removeClass("hovered");
-//       $(window).unbind("mousemove");
-//     }
-//   })
-// })
 
 
 
