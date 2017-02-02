6a9f21b5c137b0a74fc4666a6ac325d8c3386e8c
Tiffany Poss <tiffanyposs@gmail.com>
2017-02-01 23:18:45 -0500

    80

--- app.js
+++ app.js
@@ -36,31 +36,7 @@ app.use(express.static('public'));
 
 
 
-	// // development error handler
-	// // will print stacktrace
-	// if (app.get('env') === 'development') {
-	//     app.use(function(err, req, res, next) {
-	//         res.status(err.status || 500);
-	//         res.render('error', {
-	//             message: err.message,
-	//             error: err
-	//         });
-	//     });
-	// }
-
-	// // production error handler
-	// // no stacktraces leaked to user
-	// app.use(function(err, req, res, next) {
-	//     res.status(err.status || 500);
-	//     res.render('error', {
-	//         message: err.message,
-	//         error: {}
-	//     });
-	// });
-
-
-
-app.listen(8081)
+app.listen(80)
 
 console.log("I love motorcycles on port 8081")
 
